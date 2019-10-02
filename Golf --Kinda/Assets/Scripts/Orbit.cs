2f5369d427427b9b4ea2c2a732df5b2997fc1dac
Ciaran Malone <ciaranmalone27@gmail.com>
2019-10-02 23:41:43 +0100

    MOvement, Camera and terrain added

---
+++ Golf --Kinda/Assets/Scripts/Orbit.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public float xSpread;
+    public float zSpread;
+    public float yOffset;
+    public Transform centerPoint;
+
+    public float rotSpeed;
+    public bool rotateClockwise;
+
+    float timer = 0;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime * rotSpeed;
+       
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotSpeed = 5;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotSpeed = -5;
+        }
+
+        else
+        {
+            rotSpeed = 0;
+        }
+
+        Rotate();
+        transform.LookAt(centerPoint);
+    }
+
+    void Rotate()
+    {
+        if (rotateClockwise)
+        {
+            float x = -Mathf.Cos(timer) * xSpread;
+            float z = Mathf.Sin(timer) * zSpread;
+            Vector3 pos = new Vector3(x, yOffset, z);
+            transform.position = pos + centerPoint.position;
+        }
+        else
+        {
+            float x = Mathf.Cos(timer) * xSpread;
+            float z = Mathf.Sin(timer) * zSpread;
+            Vector3 pos = new Vector3(x, yOffset, z);
+            transform.position = pos + centerPoint.position;
+        }
+    }
+}
+
---
+++ Golf --Kinda/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody rb;
+
+    public Transform Target;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 cam = Target.forward * -1;
+            Vector3 camFwd = new Vector3(cam.x, 0, cam.z);
+            rb.AddForce(camFwd * speed);
+        }
+    }
+}

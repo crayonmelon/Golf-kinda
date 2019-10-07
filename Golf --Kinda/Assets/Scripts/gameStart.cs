0640440126da904c392368ba820108505329501e
Ciaran Malone <ciaranmalone27@gmail.com>
2019-10-07 12:11:33 +0100

    Week 5 work

---
+++ Golf --Kinda/Assets/Scripts/gameStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameStart : MonoBehaviour
+{
+    public GameObject StartingPoint;
+
+    void Start()
+    {
+        transform.position = StartingPoint.transform.position;
+    }
+
+}

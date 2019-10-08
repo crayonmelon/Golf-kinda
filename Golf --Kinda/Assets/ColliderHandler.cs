f6e0ced4f5b00d84d00dbe26792bda5a2d140595
Ciaran Malone <ciaranmalone27@gmail.com>
2019-10-08 21:18:19 +0100

    Water done, and some other texture stuff

---
+++ Golf --Kinda/Assets/ColliderHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderHandler : MonoBehaviour
+{
+    public float thrust = 200000f;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "GolfClub")
+        {
+       
+
+            Vector3 cam = collision.transform.forward;
+            Vector3 camFwd = new Vector3(cam.x, cam.y, cam.z);
+            rb.AddForce(camFwd * thrust, ForceMode.Impulse);
+        }
+    }
+}
--- Golf --Kinda/Assets/rotateOnPivot.cs
+++ Golf --Kinda/Assets/rotateOnPivot.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class rotateOnPivot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float RotateSpeed = 2f;
+  
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 3);
+        transform.Rotate(RotateSpeed, 0, 0);
     }
 }
